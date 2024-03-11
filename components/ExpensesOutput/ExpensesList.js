1a4a7cd1368033882d0d89cf4dc17e1a720c1897
awriyou <febriaryo@gmail.com>
2024-03-12 00:23:28 +0700

    feat : create a main component for expenses

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,9 @@
+import { FlatList, View } from "react-native";
+
+const ExpensesList = () => {
+    return (
+        <FlatList />
+    );
+}
+
+export default ExpensesList;
---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const ExpensesSummary = () => {
+    return (
+      <View>
+        <Text>PERIOD</Text>
+        <Text>SOME PRICE</Text>
+      </View>
+    );
+}
+
+export default ExpensesSummary;
