0dda6cee1a49f980fce428465daa847096ff209e
awriyou <febriaryo@gmail.com>
2024-03-12 14:47:15 +0700

    style : improve app layout n styling

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constant/styles";
 
 const ExpensesSummary = ({expenses, periodName }) => {
     const expensesSum = expenses.reduce((sum, expense) => {
@@ -10,12 +11,32 @@ const ExpensesSummary = ({expenses, periodName }) => {
     }).format(expensesSum);
 
     return (
-      <View>
-        <Text>{periodName}</Text>
-        <Text>Rp {formattedAmount}</Text>
+      <View style={styles.container}>
+        <Text style={styles.period}>{periodName}</Text>
+        <Text style={styles.sum}>{formattedAmount}</Text>
         {/* <Text>Rp {expensesSum.toFixed(2)}</Text> */}
       </View>
     );
 }
 
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 8,
+        backgroundColor: GlobalStyles.colors.forth,
+        borderRadius: 6,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    period: {
+        fontSize: 12,
+        color: GlobalStyles.colors.primary,
+    },
+    sum: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: GlobalStyles.colors.primary,
+    }
+})
