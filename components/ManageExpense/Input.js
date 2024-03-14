55c2261985861f448f5c2048b3808228050f5977
awriyou <febriaryo@gmail.com>
2024-03-15 01:34:37 +0700

    style : adding style by default styleSheet into a input component

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,13 +1,42 @@
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { GlobalStyles } from '../../constant/styles';
 
 const Input = ({ label, textInputConfig }) => {
+  const inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  } //! kondisi ini akan mengatur, jika input component digunakan untuk input multiline
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
       {/* <TextInput keyboardType={type} maxLength={maxLength}/> //! ini akan diubah saja menjadi spreadOperator (object props) */}
-      <TextInput {...textInputConfig} />
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 };
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.fiveth,
+    color: GlobalStyles.colors.secondary,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+});
