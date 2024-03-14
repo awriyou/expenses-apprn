d41c2f011042cbb44f79ddefe8a8988a5218b006
awriyou <febriaryo@gmail.com>
2024-03-15 04:33:37 +0700

    feat : providing visual validation for feedback n finishing style touch

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -13,25 +13,39 @@ const ExpenseForm = ({
 }) => {
   //   const [amountValue, setAmountValue] = useState('') //! NOTE : VALUE UNTUK FORM HARUS TETAP STRING, NANTI BARU DI KONVERSI KE YANG DINGINKAN
   //? state diatas tidak perlu digunakan lagi karena kita akan menggunakan state yang lebih scalable, yaitu dengan cara dibawah ini
-  const [inputValues, setInputValues] = useState({
-    amount: defaultValues ? defaultValues.amount.toString() : '',
-    date: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : '', //kenapa 0 dan 10? karena yang kita ingin ambil dari konversi objet ke string adalah abjad ke 0 sampai 10
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : '',
+      //   isValid: defaultValues ? true : false, //!atau kita dapat mengubahnya ke ternary
+      //   isValid: !!defaultValues, //ubah nilai ini menjadi true, agar tidak muncul terlebih dahulu saat add expense (karena nilai nya masih string kosong)
+      isValid: true,
+    },
+    // date: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : '', //kenapa 0 dan 10? karena yang kita ingin ambil dari konversi objet ke string adalah abjad ke 0 sampai 10
     //! atau kita gunakan getFormattedDate yang sudah dibuat pada utils yg sudah dibuat sebelumnya
-    date: defaultValues ? getFormattedDate(defaultValues.date) : '',
-    description: defaultValues ? defaultValues.description : '',
+    date: {
+      value: defaultValues ? getFormattedDate(defaultValues.date) : '',
+      isValid: true,
+    },
+    description: {
+      value: defaultValues ? defaultValues.description : '',
+      isValid: true,
+    },
   });
 
   function inputChangeHandler(inputIdentifier, enteredValue) {
-    setInputValues((curInputValues) => {
-      return { ...curInputValues, [inputIdentifier]: enteredValue }; //! cara ini akan membuat kita dapat set value secara dinamis
+    setInputs((curInputs) => {
+      return {
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue, isValid: true },
+      }; //! cara ini akan membuat kita dapat set value secara dinamis
     });
   }
 
   function submitHandler() {
     const expenseData = {
-      amount: +inputValues.amount,
-      date: new Date(inputValues.date),
-      description: inputValues.description,
+      amount: +inputs.amount.value,
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
 
     //? CREATE VALIDATION
@@ -41,12 +55,27 @@ const ExpenseForm = ({
 
     if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
       //show feedback
-      Alert.alert('Invalid Input', 'Please check your input!!');
+    //   Alert.alert('Invalid Input', 'Please check your input!!');
+        setInputs((curInputs) => {
+            return {
+                amount: {value: curInputs.amount.value, isValid: amountIsValid},
+                date: {value: curInputs.date.value, isValid: dateIsValid},
+                description: {value: curInputs.description.value, isValid: descriptionIsValid},
+            }
+        })
       return;
     }
 
     onSubmit(expenseData); //! ini adalah cara untuk mengirimkan data yang dimasukan oleh user ke screen manageExpenses yang nantinya akan di proses lagi
   }
+
+  //a helper for valid declaration 3 values
+  const formIsInvalid = 
+  !inputs.amount.isValid ||
+  !inputs.date.isValid ||
+  !inputs.description.isValid 
+  
+
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your Expense</Text>
@@ -54,31 +83,39 @@ const ExpenseForm = ({
         <Input
           style={styles.rowInput}
           label="Amount"
+          invalid={!inputs.amount.isValid}
           textInputConfig={{
             keyboardType: 'decimal-pad',
             onChangeText: inputChangeHandler.bind(this, 'amount'),
-            value: inputValues.amount,
+            value: inputs.amount.value,
           }}
         />
         <Input
           style={styles.rowInput}
           label="Date"
+          invalid={!inputs.date.isValid}
           textInputConfig={{
             placeholder: 'YYYY-MM-DD',
             maxLength: 10,
             onChangeText: inputChangeHandler.bind(this, 'date'),
-            value: inputValues.date,
+            value: inputs.date.value,
           }}
         />
       </View>
       <Input
         label="Description"
+        invalid={!inputs.description.isValid}
         textInputConfig={{
           multiline: true,
           onChangeText: inputChangeHandler.bind(this, 'description'),
-          value: inputValues.description,
+          value: inputs.description.value,
         }}
       />
+      {formIsInvalid && (
+        <Text style={styles.errorText}>
+          Invalid Input Values - Please check your entered data
+        </Text>
+      )}
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={onCancel}>
           Cancel
@@ -103,6 +140,11 @@ const styles = StyleSheet.create({
     color: GlobalStyles.colors.primary,
     marginVertical: 12,
     textAlign: 'center',
+  },
+  errorText: {
+    textAlign: 'center',
+    color: GlobalStyles.colors.error,
+    margin: 8,
   },
   inputRow: {
     flexDirection: 'row',
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,15 +1,21 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { GlobalStyles } from '../../constant/styles';
 
-const Input = ({ label, style, textInputConfig }) => {
+const Input = ({ label, invalid, style, textInputConfig }) => {
   const inputStyles = [styles.input];
 
   if (textInputConfig && textInputConfig.multiline) {
     inputStyles.push(styles.inputMultiline);
   } //! kondisi ini akan mengatur, jika input component digunakan untuk input multiline
+
+  if(invalid){
+    inputStyles.push(styles.invalidInput);
+  } //! kondisi ini akan mengatur jika invalid true, maka muncul style yang diinginkan, dalam kasus ini mengubah bg menjadi merah
+
+
   return (
     <View style={[styles.inputContainer, style]}>
-      <Text style={styles.label}>{label}</Text>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>{label}</Text>
       {/* <TextInput keyboardType={type} maxLength={maxLength}/> //! ini akan diubah saja menjadi spreadOperator (object props) */}
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
@@ -39,4 +45,10 @@ const styles = StyleSheet.create({
     minHeight: 100,
     textAlignVertical: 'top',
   },
+  invalidLabel: {
+    color: GlobalStyles.colors.error
+  },
+  invalidInput:{
+    backgroundColor: GlobalStyles.colors.error,
+  }
 });
