8f3a2205c7d53be59dabf954e7d2191e5d0bbb5e
awriyou <febriaryo@gmail.com>
2024-03-15 00:53:57 +0700

    last touch

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -5,14 +5,20 @@ import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../utils/date';
 
 const RecentExpenses = () => {
-    const expensesCtx = useContext(ExpensesContext)
-    const recentExpenses = expensesCtx.expenses.filter((expense) => {
-        const today = new Date();
-        const date7DaysAgo = getDateMinusDays(today, 7);
+  const expensesCtx = useContext(ExpensesContext);
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
 
-        return (expense.date >= date7DaysAgo) && (expense.date <= today);
-    }) //! ini digunakan untuk memfilter expenses yang masuk dalam 7 hari terakhir
-  return <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />;
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  }); //! ini digunakan untuk memfilter expenses yang masuk dalam 7 hari terakhir
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses added for the last 7 days."
+    />
+  );
 };
 
 export default RecentExpenses;
